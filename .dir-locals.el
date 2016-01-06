0824292dc4f5faf538e3dea8599c241593f0fded
Diego Berrocal <cestdiego@gmail.com>
2016-01-05 19:43:57 -0500

    Make Mocha Tests run in separate buffer name

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,11 @@
 ;; .dir-locals.el
 ((nil
   .
-  ((firestarter . (async-shell-command (concat "PORT=1337 mocha " (expand-file-name "test" (projectile-project-root))))))))
+  ((firestarter . (async-shell-command (concat "PORT=1337 mocha "
+                                               (expand-file-name
+                                                "test"
+
+                                                (projectile-project-root)))
+                                       "* Mocha Test Output *"
+                                       "* Mocha Test Error Output *"
+                                       )))))
